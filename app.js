9cb54beb041d3bf685e4621b5488db5fff983742
Hanna Miller <hanna.miller@students.dominican.edu>
2020-10-13 14:34:37 -0700

    built starter template

---
+++ app.js
@@ -0,0 +1,20 @@
+// Initialize express
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Render the "home" layout for the main page and send the following msg
+app.get('/', (req, res) => {
+  res.render('home', { msg: 'Handlebars are Cool!' });
+})
+
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+  console.log('App listening on port 3000!')
+})
